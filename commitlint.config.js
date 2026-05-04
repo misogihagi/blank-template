db7a5f67b12ea3d9084263f78857512579077fe5
misogihagi <47350059+misogihagi@users.noreply.github.com>
2026-05-04 15:29:31 +0900

    :wrench: (commitlint): コミットメッセージ本文にADR番号があるかどうかチェックする
    #G0030

--- commitlint.config.js
+++ commitlint.config.js
@@ -41,5 +41,48 @@ module.exports = {
         return [false, `scope must be one of ${allScopes.join(", ")}`];
       },
     ],
+    "function-rules/subject-case": [
+      2,
+      "always",
+      (parsed) => {
+        const { type, scope, footer, header, raw } = parsed;
+
+        // bodyのパースがうまく行かない時がある。その時は\n\n以降をbodyとする。
+        const body =
+          !parsed.body && parsed.raw.indexOf("\n\n") != -1
+            ? parsed.raw.slice(parsed.raw.indexOf("\n\n") + 2)
+            : parsed.body;
+
+        // wipが含まれる場合はチェックをスキップ
+        if (raw.toLowerCase().includes("wip")) {
+          return [true];
+        }
+
+        // typeが:scroll:の場合scopeはadr
+        if (type == ":scroll:") {
+          if (scope == "(adr)") {
+            return [true];
+          } else {
+            return [false, "When type is :scroll:, scope must be adr"];
+          }
+        }
+
+        // 特殊なType (:tada:, :scroll:) の場合はADR不要
+        const bypassTypes = [":tada:", ":scroll:"];
+        if (bypassTypes.includes(type)) {
+          return [true];
+        }
+
+        // 本文(body)またはフッターから #A1234 形式のADR番号を探す
+        const adrPattern = /#[A-Z]\d+/;
+        const hasAdr =
+          adrPattern.test(body || "") || adrPattern.test(footer || "");
+
+        return [
+          hasAdr,
+          "Please include the ADR number (e.g., #A1234) in the body. However, this excludes :tada:, :scroll:, or WIP commits.",
+        ];
+      },
+    ],
   },
 };
